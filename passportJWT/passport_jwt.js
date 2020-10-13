97eaafc412530755a35d2624e26179ff98fcf357
Eyuwankg-Swankg <messikarthik13@gmail.com>
2020-10-13 21:04:24 +0530

    Implemented JWT Auth

---
+++ passportJWT/passport_jwt.js
@@ -0,0 +1,25 @@
+require("dotenv/config");
+require("./.env");
+var JwtStrategy = require("passport-jwt").Strategy;
+var ExtractJwt = require("passport-jwt").ExtractJwt;
+
+//load person model
+const Person = require("../models/Person");
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("Nodejs");
+opts.secretOrKey = SECRET;
+module.exports = (passport) =>
+  passport.use(
+    new JwtStrategy(opts, function (jwt_payload, done) {
+      Person.findOne({ email: jwt_payload.email })
+        .then((person) => {
+          if (person) {
+            return done(null, person);
+          } else {
+            return done(null, false);
+          }
+        })
+        .catch((err) => console.log("Error in checking JWT Auth"));
+    })
+  );
