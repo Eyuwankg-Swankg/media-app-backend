e484849688f656fbfc3ab21b199ff7edd1f4a8bb
Eyuwankg-Swankg <messikarthik13@gmail.com>
2020-10-13 21:47:30 +0530

    Added jsonwebtokens

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
+require("dotenv/config");
+require("../.env");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
 
 // load Person model
 const Person = require("../models/Person");
+const { json } = require("body-parser");
 
 //@type      GET
 //@route     /auth/login
@@ -11,6 +15,49 @@ const Person = require("../models/Person");
 //@access    PUBLIC
 router.get("/login", (req, res) => {
   res.json({ loginpage: "Implement Login Page" });
+});
+
+//@type      POST
+//@route     /auth/login
+//@desc      route to login page
+//@access    PUBLIC
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  Person.findOne({ email })
+    .then((person) => {
+      if (!person) return res.json({ loginError: "User not exists" });
+      bcrypt.compare(password, person.password, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        if (result) {
+          const payload = {
+            id: person.id,
+            name: person.name,
+            email: person.email,
+          };
+          jsonwebtoken.sign(
+            payload,
+            SECRET,
+            { expiresIn: 60 * 60 },
+            (err, token) => {
+              if (err) {
+                console.log(err);
+              }
+              res.json({
+                token: "Nodejs " + token,
+              });
+            }
+          );
+        } else {
+          res.json({ passwordError: "Password does not match" });
+        }
+      });
+    })
+    .catch((err) =>
+      console.log("Error in Searching for user in Login Post route ")
+    );
 });
 
 //@type      GET
@@ -23,8 +70,8 @@ router.get("/register", (req, res) => {
 
 //@type      POST
 //@route     /auth/register
-//@desc      route to register user in DB
-//@access    PRIVATE
+//@desc      route to register a user in DB
+//@access    PUBLIC
 router.post("/register", (req, res) => {
   Person.findOne({ email: req.body.email })
     .then((person) => {
@@ -37,18 +84,21 @@ router.post("/register", (req, res) => {
           password: req.body.password,
         };
         if (req.body.userName) newperson.userName = req.body.userName;
-
-        if (req.body.profilePic) newperson.profilePic = req.body.profilePic;
-
-        if (req.body.gender) newperson.gender = req.body.gender;
-
-        bcrypt.genSalt(10, function (err, salt) {
+        newperson.profilePic = "../public/user.png";
+        if (req.body.gender) {
+          newperson.gender = req.body.gender;
+          if (newperson.gender == "male")
+            newperson.profilePic = "../public/maleUser.png";
+          else newperson.profilePic = "../public/femaleUser.png";
+        }
+        bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newperson.password, salt, function (err, hash) {
             newperson.password = hash;
             const newPerson = new Person(newperson);
             newPerson
               .save()
               .then((person) => {
+                console.log("Saved to Database");
                 res.json(person);
               })
               .catch((err) => console.log("Error while registering"));
@@ -57,7 +107,7 @@ router.post("/register", (req, res) => {
       }
     })
     .catch((err) =>
-      console.log("Error while checking wheater user in db or not")
+      console.log("Error in checking for user while registering")
     );
 });
 
