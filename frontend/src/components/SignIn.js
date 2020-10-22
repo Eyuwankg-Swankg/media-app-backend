4139d225ff8871e8030fb0a3c383b3669bba0d76
Eyuwankg-Swankg <messikarthik13@gmail.com>
2020-10-23 01:00:11 +0530

    added sigin and signup states

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -2,11 +2,22 @@ import React, { Component } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.onsubmit = this.onsubmit.bind(this);
+  }
+  onsubmit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
       <div className="signin">
         <div>
-          <Form id="signin-form">
+          <Form id="signin-form" onSubmit={this.onsubmit}>
             <FormGroup>
               <Label
                 for="exampleEmail"
@@ -18,7 +29,13 @@ class SignIn extends Component {
               >
                 Email
               </Label>
-              <Input type="email" name="email" id="exampleEmail" />
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                value={this.state.email}
+                onChange={(e) => this.setState({ email: e.target.value })}
+              />
             </FormGroup>
             <FormGroup>
               <Label
@@ -31,7 +48,13 @@ class SignIn extends Component {
               >
                 Password
               </Label>
-              <Input type="password" name="password" id="examplePassword" />
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                value={this.state.password}
+                onChange={(e) => this.setState({ password: e.target.value })}
+              />
             </FormGroup>
             <Button
               style={{
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+    this.onsubmit = this.onsubmit.bind(this);
+  }
+  onsubmit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
       <div>
         <div className="signin">
           <div>
-            <Form id="signin-form">
+            <Form id="signin-form" onSubmit={this.onsubmit}>
               <FormGroup>
                 <Label
                   for="name"
@@ -19,7 +31,13 @@ class SignUp extends Component {
                 >
                   Name
                 </Label>
-                <Input type="text" name="name" id="name" />
+                <Input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={this.state.name}
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                />
               </FormGroup>
 
               <FormGroup>
@@ -33,7 +51,13 @@ class SignUp extends Component {
                 >
                   Email
                 </Label>
-                <Input type="email" name="email" id="exampleEmail" />
+                <Input
+                  type="email"
+                  name="email"
+                  id="exampleEmail"
+                  value={this.state.email}
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                />
               </FormGroup>
               <FormGroup>
                 <Label
@@ -46,7 +70,13 @@ class SignUp extends Component {
                 >
                   Password
                 </Label>
-                <Input type="password" name="password" id="examplePassword" />
+                <Input
+                  type="password"
+                  name="password"
+                  id="examplePassword"
+                  value={this.state.password}
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                />
               </FormGroup>
               <Button
                 style={{
