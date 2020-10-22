b37a284afafb0498a80f49c1c4a4af893300106e
Eyuwankg-Swankg <messikarthik13@gmail.com>
2020-10-22 22:36:00 +0530

    Added redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <SignUp />
+      </div>
+    </Provider>
   );
 }
 
